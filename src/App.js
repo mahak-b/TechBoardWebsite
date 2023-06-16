0d64b590976cae322cf4ad0621b60c6ef1dee1ad
botketan <ketansingh918@gmail.com>
2023-06-17 00:41:44 +0530

    uploading navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Navbar></Navbar>
+    </div>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+export default function Navbar() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', changeWidth)
+    return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  },[])
+
+
+
+  return (
+    <div className='navbar'>
+        <div className='navigation'>      
+      {(screenWidth > 800) && (<a  href= "/" id = 'Navbar-left' className='link-item'>TECHBOARD.</a> )}
+      {(screenWidth <= 800) && (<img src = {require('../assests/navbar-left.png')} alt ="Not_found" className='link-item'></img> )}
+        <a href= "/" className='link-item'>CLUBS</a>
+        <a href= "/" className='link-item'>EVENTS</a>
+        <a href= "/" className='link-item'>TEAM</a>
+        <a href= "/" className='link-item'>DOTLINK</a>
+        </div>
+        <div className='navbar-right'>
+          <img src = {require('../assests/NavbarRight.jpg')} alt ="Not_found"  id='logo'></img>
+        </div>
+
+    </div>
+    
+  );
+}
