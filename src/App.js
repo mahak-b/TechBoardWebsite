415be5eb5517a698d89a82466fb56c984f65f365
Paridhi Baruah <paridhibaruah2002@gmail.com>
2023-06-20 16:37:55 +0530

    Club and News page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-
 import './App.css';
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar';
+import Club from './Components/Club/Club';
+import News from './Components/News/News';
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div>
       <Navbar></Navbar>
+      <Switch>
+          <Route path="/Club">
+            <Club/>
+          </Route>
+          <Route path="/News">
+            <News/>
+          </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
