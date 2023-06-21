92e29d02630229e6902c69d7c4fd3c6daaec63da
JoNTy <zx12345qvm@gmail.com>
2023-06-22 00:48:42 +0530

    added more project sections

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -5,7 +5,7 @@ import minus from "../assests/minus.png";
 import projectImage1A from "../assests/image 17.png";
 import projectImage1B from "../assests/image 18.png";
 import projectImage1C from "../assests/image 25.png";
-
+import arrow from "../assests/arrow.png";
 
 const Projects = () => {
     const [project, useProject] = useState([
@@ -16,8 +16,24 @@ const Projects = () => {
             name:'PROJECT#2.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:2},
 
         {
-            name:'AUTOMOTIVE CLUB.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:3
+            name:'PROJECT#3.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:3
+        },
+        {
+            name:'PROJECT#4.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:4
+        },
+        {
+            name:'PROJECT#5.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:5
+        },
+        {
+            name:'PROJECT#6.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:6
+        },
+        {
+            name:'PROJECT#7.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:7
+        },
+        {
+            name:'PROJECT#8.',data:"The start of this tenure too seemed as bleak as last, with the second wave still raging on and another online semester on the horizon, we decided a quizzing event was due to liven everyone’s evenings, which lead to colligere - weekends full of quizzes based on a myriad of topics to satisfy one and all ranging from Football to memes. All in all we had 13 quizzes hosted on our facebook group involving a lot of fast typing, furious refreshing of the web page and lightning fast answers.Through this multi-month extravaganza, a ton of people discovered how much fun quizzing could be. In the end, that's what it was all about, encapsulating the spirit of Acumen",id:8
         }
+
     ]);
 
     const[expandedProjectId, setExpandedProjectId] = useState(null);
@@ -34,8 +50,8 @@ const Projects = () => {
     return (
         <div className="project-list">
             <div className="project-header">
-                <div className="project-first-line">    PROJECTS    PROJECTS</div>
-                <div className="project-second-line">JECTS    PROJECTS    PRO</div>
+                <div className="project-first-line">    PROJECTS    PROJECTS    </div>
+                <div className="project-second-line">JECTS    PROJECTS    PROJECTS</div>
             </div>
             {project.map((project)=>(
                 <div className="project-name" key={project.id}>
@@ -49,7 +65,7 @@ const Projects = () => {
                     </div>
                     {expandedProjectId === project.id && (<div className="project-overview">
                         <div className="project-data">
-                            <p>{club.data}</p>
+                            <p>{project.data}</p>
                         </div>
                         <div className="project-image">
                             <div className="project-blank">
@@ -63,9 +79,11 @@ const Projects = () => {
                                     </button>
                                 </div>
                             </div>
+                            <div className="project-image-container">
                             <img src={projectImage1A} className="project-img1" alt=""/>
-                            <img src={projectImage1B} className="project-img2" alt=""/>
                             <img src={projectImage1C} className="project-img3" alt=""/>
+                            <img src={projectImage1B} className="project-img2" alt=""/>
+                            </div>
                         </div>
                 </div>)}
             </div>
